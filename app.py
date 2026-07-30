1965b21c9474d2993ec71c7f3eabd4faa6c67d7f
mannan1821 <gamersage1810@gmail.com>
2026-07-30 10:32:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ st.title("AI RESUME GENERATION")
 st.write("""This app helps user to build customized professional Resume
 with Latest Job apply links""")
 
-st.image("bg.png")
+st.image("osama.png")
 st.sidebar.title("Fill Important Details")
-st.sidebar.image("bg.png")
+st.sidebar.image("osama.png")
 
 TAVILY_API_KEY = "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"
 GOOGLE_API_KEY = "AQ.Ab8RN6JWAS9vcT6ZtC2ESg_rNIZeGqWYdsVFLhUTgJ3htX3LuA"
