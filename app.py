db08dd355a98f0d837e11c0a6bf6723fe8f0a025
mannan1821 <gamersage1810@gmail.com>
2026-07-31 09:04:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -157,6 +157,6 @@ if st.button("Generate Resume"):
         st.html(code, width = "stretch",
                unsafe_allow_javascript = True)
         st.divider()
-        jobe_code = get_jobs(agent, location, profile)
+        job_code = get_jobs(agent, location, profile)
         st.html(job_code , width="stretch" ,
                 unsafe_allow_javascript=True)
