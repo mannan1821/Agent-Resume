623e7bf335266cb548d9c6041a513cd669ea49a9
mannan1821 <gamersage1810@gmail.com>
2026-07-31 09:25:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ elif all(all_API):
     model = ChatGoogleGenerativeAI(
     model = 'gemini-3.5-flash-lite',
     google_api_key = GOOGLE_API_KEY
-)
+    )
 else:
     st.info("PASS ALL API_KEYS")
 
